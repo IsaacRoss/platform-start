e2449263350a26565c99932013bd02a49f3037a8
Isaac Ross <isaac253@gmail.com>
2017-06-29 22:03:41 -0400

    working elm app

---
+++ lib/platform/web/views/layout_view.ex
@@ -0,0 +1,21 @@
+defmodule Platform.Web.LayoutView do
+  use Platform.Web, :view
+
+  def js_script_tag do
+    if Mix.env == :prod do
+      # In production we'll just reference the file
+      "<script src=\"/js/app.js\"></script>"
+    else
+      # In development mode we'll load it from our webpack dev server
+      "<script src=\"http://localhost:8080/js/app.js\"></script>"
+    end
+  end
+
+  def css_link_tag do
+    if Mix.env == :prod do
+      "<link rel=\"stylesheet\" type=\"text/css\" href=\"/css/app.css\" />"
+    else
+      "<link rel=\"stylesheet\" type=\"text/css\" href=\"http://localhost:8080/css/app.css\" />"
+    end
+  end
+end
